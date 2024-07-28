d216b4b3fad248a011f7472f6ab2e48fa2ee1e0e
LGI4 <lucianosoccer4@gmail.com>
2024-07-28 17:15:52 -0400

    using plotly instead of matplotlib

--- analysis/descriptive_analysis.py
+++ analysis/descriptive_analysis.py
@@ -1,14 +1,27 @@
 import pandas as pd
+import plotly
 
 # Load the processed data
 data_path = 'data/processed/processed_data.csv'
 df = pd.read_csv(data_path)
 
 # Perform descriptive statistics
-df = df.describe()
+def summary_statistics(df):
+    df.info()  
+    print(df.describe())
+    return
+
+def plot (df, columns, output_dir):
+    return
+
+
+
+
+
 
 # Save the summary statistics
 output_file = 'data/outputs/descriptive_analysis.csv'
 # df.to_csv(output_file)
 df.to_csv(output_file)
 print("Descriptive analysis saved.")
+
--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# Assuming each script has a main function that executes the necessary tasks
-
 # Step 1: Extract Data
 print("Running Extract Step")
 os.system("python etl/extract.py")
@@ -18,20 +15,12 @@ os.system("python etl/extract.py")
 print("Running Transform Step")
 os.system("python etl/transform.py")
 
-# Step 3: Load Data
-print("Running Load Step")
-os.system("python etl/load.py")
-
-# Step 4: Model Data
-print("Running Model Step")
-os.system("python analysis/model.py")
-
-# Step 5: Evaluate Model
-print("Running Evaluate Step")
+# Step 3: Evaluate Model
+print("Running Evaluation")
 os.system("python analysis/evaluate.py")
 
-# Step 6: Generate Visualizations
-print("Running Visualization Step")
+# Step 4: Generate Visualizations
+print("Running Visualization")
 os.system("python vis/visualizations.py")
 
-print("Pipeline execution completed.")
+print("Finished running.")
